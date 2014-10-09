626b83e12b5375b562b6c3f09c4b18d1e1c98af7
Thomas Guignard <tom@timtom.ch>
2014-10-08 23:46:47 -0400

    Adapted adafruit sample logger for my circuit

--- datalogging_test/datalogging_test.ino
+++ datalogging_test/datalogging_test.ino
@@ -2,14 +2,44 @@
   Adapted from http://www.ladyada.net/make/logshield/lighttemp.html
   */
 
+#include <SD.h>
+#include <Wire.h>
+#include "RTClib.h"
+
+// A simple data logger for the Arduino analog pins
+
+// how many milliseconds between grabbing data and logging it. 1000 ms is once a second
+#define LOG_INTERVAL  1000 // mills between entries (reduce to take more/faster data)
+
+// how many milliseconds before writing the logged data permanently to disk
+// set it to the LOG_INTERVAL to write each time (safest)
+// set it to 10*LOG_INTERVAL to write all data every 10 datareads, you could lose up to 
+// the last 10 reads if power is lost but it uses less power and is much faster!
+#define SYNC_INTERVAL 1000 // mills between calls to flush() - to write data to the card
+uint32_t syncTime = 0; // time of last sync()
+
+#define ECHO_TO_SERIAL   1 // echo data to serial port
+#define WAIT_TO_START    0 // Wait for serial input in setup()
+
+// the digital pins that connect to the LEDs
+#define redLEDpin 2
+#define greenLEDpin 3
+
+// The analog pins that connect to the sensors
+#define photocellPin 0           // analog 0
+#define tempPin 1                // analog 1
+#define BANDGAPREF 14            // special indicator that we want to measure the bandgap
+
 #define aref_voltage 3.3         // we tie 3.3V to ARef and measure it with a multimeter!
+#define bandgap_voltage 1.1      // this is not super guaranteed but its not -too- off
 
-int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
-int photocellReading;     // the analog reading from the analog resistor divider
+RTC_DS1307 RTC; // define the Real Time Clock object
 
-//Differently than in the Adafruit circuit example, the temp sensor here is a thermistor
-int tempPin = 1;        // V_out from the thermistor voltage divider is connected to a1
-int tempReading;        // the analog reading from the sensor
+// for the data logging shield, we use digital pin 10 for the SD cs line
+const int chipSelect = 10;
+
+// the logging file
+File logfile;
 
 // This function will map measured voltage from the lookup table
 // source: http://playground.arduino.cc/Main/MultiMap
@@ -38,55 +68,192 @@ float FmultiMap(float val, float * _in, float * _out, uint8_t size)
 float voltage_values[]={0,0.01,0.01,0.01,0.02,0.02,0.04,0.05,0.07,0.09,0.13,0.17,0.22,0.28,0.36,0.45,0.55,0.67,0.8,0.94,1.09,1.25,1.41,1.57,1.73,1.88,2.03,2.16,2.28,2.4,2.5,2.59,2.67,2.74,2.81,2.86,2.91};
 float temp_values[]={-55,-50,-45,-40,-35,-30,-25,-20,-15,-10,-5,0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125};
     
-void setup(void) {
-  // We'll send debugging information via the Serial monitor
-  Serial.begin(9600);   
 
+
+void error(char *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+  
+  // red LED indicates error
+  digitalWrite(redLEDpin, HIGH);
+
+  while(1);
+}
+
+void setup(void)
+{
+  Serial.begin(9600);
+  Serial.println();
+  
+  // use debugging LEDs
+  pinMode(redLEDpin, OUTPUT);
+  pinMode(greenLEDpin, OUTPUT);
+  
+#if WAIT_TO_START
+  Serial.println("Type any character to start");
+  while (!Serial.available());
+#endif //WAIT_TO_START
+
+  // initialize the SD card
+  Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    error("Card failed, or not present");
+  }
+  Serial.println("card initialized.");
+  
+  // create a new file
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+  
+  if (! logfile) {
+    error("couldnt create file");
+  }
+  
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+
+  // connect to RTC
+  Wire.begin();  
+  if (!RTC.begin()) {
+    logfile.println("RTC failed");
+#if ECHO_TO_SERIAL
+    Serial.println("RTC failed");
+#endif  //ECHO_TO_SERIAL
+  }
+  
+
+  logfile.println("millis,stamp,datetime,light,temp,vcc");    
+#if ECHO_TO_SERIAL
+  Serial.println("millis,stamp,datetime,light,temp,vcc");
+#endif //ECHO_TO_SERIAL
+ 
   // If you want to set the aref to something other than 5v
   analogReference(EXTERNAL);
 }
 
+void loop(void)
+{
+  DateTime now;
 
-void loop(void) {
-  photocellReading = analogRead(photocellPin);  
+  // delay for the amount of time we want between readings
+  delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
   
-  Serial.print("Light reading = ");
-  Serial.print(photocellReading);     // the raw analog reading
+  digitalWrite(greenLEDpin, HIGH);
   
-  // We'll have a few threshholds, qualitatively determined
-  if (photocellReading < 10) {
-    Serial.println(" - Dark");
-  } else if (photocellReading < 200) {
-    Serial.println(" - Dim");
-  } else if (photocellReading < 500) {
-    Serial.println(" - Light");
-  } else if (photocellReading < 800) {
-    Serial.println(" - Bright");
-  } else {
-    Serial.println(" - Very bright");
-  }
+  // log milliseconds since starting
+  uint32_t m = millis();
+  logfile.print(m);           // milliseconds since start
+  logfile.print(", ");    
+#if ECHO_TO_SERIAL
+  Serial.print(m);         // milliseconds since start
+  Serial.print(", ");  
+#endif
+
+  // fetch the time
+  now = RTC.now();
+  // log time
+  logfile.print(now.unixtime()); // seconds since 1/1/1970
+  logfile.print(", ");
+  logfile.print('"');
+  logfile.print(now.year(), DEC);
+  logfile.print("/");
+  logfile.print(now.month(), DEC);
+  logfile.print("/");
+  logfile.print(now.day(), DEC);
+  logfile.print(" ");
+  logfile.print(now.hour(), DEC);
+  logfile.print(":");
+  logfile.print(now.minute(), DEC);
+  logfile.print(":");
+  logfile.print(now.second(), DEC);
+  logfile.print('"');
+#if ECHO_TO_SERIAL
+  Serial.print(now.unixtime()); // seconds since 1/1/1970
+  Serial.print(", ");
+  Serial.print('"');
+  Serial.print(now.year(), DEC);
+  Serial.print("/");
+  Serial.print(now.month(), DEC);
+  Serial.print("/");
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(":");
+  Serial.print(now.minute(), DEC);
+  Serial.print(":");
+  Serial.print(now.second(), DEC);
+  Serial.print('"');
+#endif //ECHO_TO_SERIAL
+
+  analogRead(photocellPin);
+  delay(10); 
+  int photocellReading = analogRead(photocellPin);  
   
-  tempReading = analogRead(tempPin);  
+  analogRead(tempPin); 
+  delay(10);
+  int tempReading = analogRead(tempPin);    
   
-  Serial.print("Temp reading = ");
-  Serial.print(tempReading);     // the raw analog reading
-  
-  // converting that reading to voltage, which is based off the reference voltage
-  float voltage = tempReading * aref_voltage / 1024; 
- 
-  // print out the voltage
-  Serial.print(" - ");
-  Serial.print(voltage); Serial.println(" volts");
- 
-  // now print out the temperature
-  
+  // converting that reading to voltage, based on the reference voltage
+  float voltage = tempReading * aref_voltage / 1024;
+
+  // to convert into temperature reading, use the lookup table
   float temperatureC;
   temperatureC = FmultiMap((float)voltage, voltage_values, temp_values, 37);
   
-  Serial.print(temperatureC); Serial.println(" degrees C");
- 
+  logfile.print(", ");    
+  logfile.print(photocellReading);
+  logfile.print(", ");    
+  logfile.print(temperatureC);
+#if ECHO_TO_SERIAL
+  Serial.print(", ");   
+  Serial.print(photocellReading);
+  Serial.print(", ");    
+  Serial.print(temperatureC);
+#endif //ECHO_TO_SERIAL
 
- 
-  delay(1000);
+  // Log the estimated 'VCC' voltage by measuring the internal 1.1v ref
+  analogRead(BANDGAPREF); 
+  delay(10);
+  int refReading = analogRead(BANDGAPREF); 
+  float supplyvoltage = (bandgap_voltage * 1024) / refReading; 
+  
+  logfile.print(", ");
+  logfile.print(supplyvoltage);
+#if ECHO_TO_SERIAL
+  Serial.print(", ");   
+  Serial.print(supplyvoltage);
+#endif // ECHO_TO_SERIAL
+
+  logfile.println();
+#if ECHO_TO_SERIAL
+  Serial.println();
+#endif // ECHO_TO_SERIAL
+
+  digitalWrite(greenLEDpin, LOW);
+
+  // Now we write data to disk! Don't sync too often - requires 2048 bytes of I/O to SD card
+  // which uses a bunch of power and takes time
+  if ((millis() - syncTime) < SYNC_INTERVAL) return;
+  syncTime = millis();
+  
+  // blink LED to show we are syncing data to the card & updating FAT!
+  digitalWrite(redLEDpin, HIGH);
+  logfile.flush();
+  digitalWrite(redLEDpin, LOW);
+  
 }
 
